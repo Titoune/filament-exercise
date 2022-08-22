87ae49abec421817ee36164ab0b97d17ce08dd87
adeliry <adeliry@imedia.ch>
2022-08-22 22:41:59 +0200

    migrations/seeds corrections

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         \App\Models\Category::factory(4)->create();
         \App\Models\SubCategory::factory(20)->create();
+
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'example@example.ch',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password')
+        ]);
     }
 }
